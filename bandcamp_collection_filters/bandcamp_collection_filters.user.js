a383d0a35bb265a5e950fc0dbd2f46c2b9858ab6
Ryan Liptak <squeek502@hotmail.com>
2019-10-21 23:53:32 -0700

    Initial commit

---
+++ bandcamp_collection_filters/bandcamp_collection_filters.user.js
@@ -0,0 +1,217 @@
+// ==UserScript==
+// @name Bandcamp Collection Filters
+// @version 1.0.0
+// @description List items in a collection or wishlist that match certain filters (free, in common, etc)
+// @namespace 289690-squeek502
+// @license Unlicense
+// @match http*://bandcamp.com/*
+// @include http*://bandcamp.com/*
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+  var document = window.top.document;
+  if (!document.querySelector('#collection-grid') || !document.querySelector('#wishlist-grid'))
+    return;
+
+  var collectionSummary;
+  var pageData;
+  var isOwner = document.querySelector('#fan-banner').classList.contains('owner');
+  var LOAD_URL_FORMAT = "https://bandcamp.com/api/fancollection/1/{}_items";
+
+  var pageTypes = ['collection', 'wishlist'];
+  var buttonTypes = ['free', 'purchased', 'wishlisted'];
+
+  var inCommonSeparator = document.createElement('span');
+  inCommonSeparator.style.color = '#828282';
+  inCommonSeparator.style.marginRight = '16px';
+  inCommonSeparator.textContent = 'in common:';
+  var separatorsAfter = {'free': inCommonSeparator};
+
+  var buttons = {};
+  var results = {};
+  var started = {};
+
+  pageTypes.forEach(function(type) {
+    buttons[type] = {};
+    results[type] = {};
+
+    var buttonContainer = document.createElement('div');
+    buttonContainer.style.marginTop = '0px';
+    buttonContainer.classList.add('wishlist-controls');
+    buttonContainer.classList.add('owner-controls');
+
+    var grid = document.querySelector('#'+type+'-grid');
+    var itemsContainer = grid.querySelector('#'+type+'-items-container') || grid.querySelector(':scope > .inner');
+    var items = itemsContainer.querySelector('#'+type+'-items');
+
+    var resultContainer = document.createElement('div');
+
+    buttonTypes.forEach(function(button) {
+      var buttonElement = document.createElement('a');
+      buttonElement.style.marginRight = '16px';
+      buttonElement.innerHTML = button;
+      buttonContainer.appendChild(buttonElement);
+
+      var list = document.createElement('ul');
+      list.style.display = 'none';
+      resultContainer.appendChild(list);
+
+      buttons[type][button] = buttonElement;
+      results[type][button] = list;
+
+      if (separatorsAfter[button]) {
+        buttonContainer.appendChild(separatorsAfter[button].cloneNode(true));
+      }
+    });
+
+    itemsContainer.insertBefore(buttonContainer, items);
+    itemsContainer.insertBefore(resultContainer, items);
+  });
+
+  var onSummaryError = function(errmsg) {
+    pageTypes.forEach(function(type) {
+      buttonTypes.forEach(function(button) {
+        // free doesn't depend on summary
+        if (button == 'free') return;
+        results[type][button].innerHTML = errmsg;
+      });
+    });
+  };
+
+  var handleItems = function(items, type) {
+    items.forEach(function(item) {
+      var isFree = item.price === 0;
+      var lookupKey = item.tralbum_type + "" + item.tralbum_id;
+      var tralbum = collectionSummary && collectionSummary.tralbum_lookup[lookupKey];
+      var isPurchased = tralbum && tralbum.purchased;
+      var isWishlisted = tralbum && !tralbum.purchased;
+      if (!(isFree || isPurchased || isWishlisted))
+        return;
+
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      a.href = item.item_url;
+      a.textContent = item.band_name + ' - ' + item.item_title;
+      li.appendChild(a);
+
+      if (isFree) {
+        results[type].free.appendChild(li.cloneNode(true));
+      }
+      if (isPurchased) {
+        results[type].purchased.appendChild(li.cloneNode(true));
+      }
+      if (isWishlisted) {
+        results[type].wishlisted.appendChild(li.cloneNode(true));
+      }
+    });
+    buttonTypes.forEach(function(button) {
+       buttons[type][button].textContent = button + " (" + results[type][button].childElementCount + ")";
+    });
+  };
+
+  var get = function(url, cb) {
+    var opts = {
+      method: 'GET',
+      url: url,
+      onload: function (res) {
+        cb(res.status, res.responseText, res.finalUrl || url);
+      }
+    };
+    GM_xmlhttpRequest(opts);
+  };
+
+  var post = function(url, data, cb) {
+    var opts = {
+      method: 'POST',
+      url: url,
+      data: data,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      onload: function (res) {
+        cb(res.status, res.responseText, res.finalUrl || url);
+      }
+    };
+    GM_xmlhttpRequest(opts);
+  };
+
+  var getNext = function(fan_id, older_than_token, type) {
+    var url = LOAD_URL_FORMAT.replace('{}', type);
+    post(url, '{"fan_id":'+fan_id+',"older_than_token":"'+older_than_token+'","count":40}', function(status, res, url) {
+      if (status != 200) {
+        console.error("failed to get next " + type, status, res, url);
+        return;
+      }
+      var parsed = JSON.parse(res);
+      if (parsed.error) {
+        console.error("error when getting next " + type, parsed.error_message, parsed);
+        return;
+      }
+      handleItems(parsed.items, type);
+      if (parsed.more_available) {
+        getNext(fan_id, parsed.last_token, type);
+      }
+    });
+  };
+
+  var setState = function(type, button, state) {
+    if (state) {
+      // hide all of the same type
+      buttonTypes.forEach(function(other) {
+        if (other == button) return;
+        setState(type, other, false);
+      });
+      results[type][button].style.display = 'block';
+      buttons[type][button].style.fontWeight = 'bold';
+      buttons[type][button].style.textDecoration = 'underline';
+    } else {
+      results[type][button].style.display = 'none';
+      buttons[type][button].style.fontWeight = 'normal';
+      buttons[type][button].style.textDecoration = 'none';
+    }
+  };
+
+  var getState = function(type, button) {
+    return results[type][button].style.display != 'none';
+  };
+
+  var onclick = function(type, button, e) {
+    if (!started[type]) {
+      if (!pageData) {
+        pageData = JSON.parse(document.querySelector('#pagedata').getAttribute('data-blob'));
+      }
+      var start = function() {
+        var now = Math.floor(Date.now()/1000);
+        var nowToken = pageData[type+'_data'].last_token.replace(/^\d+/, now);
+        getNext(pageData.fan_data.fan_id, nowToken, type);
+      };
+      if (!collectionSummary) {
+        get('https://bandcamp.com/api/fan/2/collection_summary', function(status, res, url) {
+          if (status != 200) {
+            console.warn("unexpected response from " + url, status, res);
+            onSummaryError("unexpected http status code: " + status);
+          }
+          var parsedRes = JSON.parse(res);
+          if (parsedRes.error) {
+            onSummaryError(parsedRes.error_message);
+          } else {
+            collectionSummary = parsedRes.collection_summary;
+          }
+          start();
+        });
+      } else {
+        start();
+      }
+      setState(type, button, true);
+      started[type] = true;
+    } else {
+      setState(type, button, !getState(type, button));
+    }
+  };
+  pageTypes.forEach(function(type) {
+    buttonTypes.forEach(function(button) {
+      buttons[type][button].addEventListener("click", onclick.bind(null, type, button));
+    });
+  });
+})();
---
+++ bandcamp_collection_tags/bandcamp_collection_tags.user.js
@@ -0,0 +1,119 @@
+// ==UserScript==
+// @name Bandcamp Album Tags On Collection Pages
+// @version 1.0.2
+// @description Shows album tags when hovering over an album on a collection page
+// @namespace 289690-squeek502
+// @license Unlicense
+// @match http*://bandcamp.com/*
+// @include http*://bandcamp.com/*
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+  var document = window.top.document;
+  var tags = {};
+  var loadings = {};
+  var albumSelector = '.collection-items .collection-item-container[data-tralbumid]';
+  var svgData = '<svg width="38" height="38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g transform="translate(1 1)" stroke-width="2" fill="none" fill-rule="evenodd"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></svg>';
+
+  var makeTags = function(container, id, href) {
+    var tags = document.createElement('div');
+    tags.style.pointerEvents = 'none';
+    tags.style.padding = '2px';
+    tags.style.position = 'absolute';
+    tags.style.top = '0px'; tags.style.left = '0px';
+
+    if (!loadings[id]) {
+      var loading = document.createElement('div');
+      loading.style.pointerEvents = 'none';
+      loading.style.padding = '4px';
+      loading.style.position = 'absolute';
+      loading.style.top = '0px'; loading.style.left = '0px';
+      loading.innerHTML = svgData;
+      container.appendChild(loading);
+      loadings[id] = loading;
+    }
+
+    // get tags
+    GM_xmlhttpRequest({
+      method: 'GET',
+      url: href,
+      onload: function (res) {
+        var status = res.status;
+        var body = res.responseText;
+        if (status != 200) {
+          console.log("Failed to get tags for: "+href, status, res);
+          return;
+        }
+        var htmlDoc = document.implementation.createHTMLDocument();
+        htmlDoc.documentElement.innerHTML = body;
+
+        container.removeChild(loadings[id]);
+
+        htmlDoc.querySelectorAll(".tralbum-tags .tag").forEach(tag => {
+          var span = document.createElement('span');
+          span.style.display = 'inline-block';
+          span.style.margin = '2px';
+          span.style.padding = '2px';
+          span.style.backgroundColor = 'rgba(255, 255, 255, 0.85)';
+          span.innerHTML = tag.innerHTML;
+          tags.appendChild(span);
+        });
+      }
+    });
+    return tags;
+  };
+
+  var setupContainer = function(container) {
+    var a = container.querySelector('a.item-link');
+    var href = a.href;
+    var id = container.getAttribute('data-tralbumid');
+    var parent = container.querySelector('.collection-item-art-container');
+
+    container.addEventListener("mouseover", function(event) {
+      if (!tags[id]) {
+        tags[id] = makeTags(container, id, href);
+      }
+      if (tags[id].parentNode != parent) {
+        parent.appendChild(tags[id]);
+      }
+      parent.style.position = 'relative';
+      tags[id].style.display = 'block';
+    });
+
+    container.addEventListener("mouseout", function(event) {
+      if (tags[id]) {
+        tags[id].style.display = 'none';
+      }
+    });
+  };
+
+  document.querySelectorAll(albumSelector).forEach(setupContainer);
+
+  // Select the node that will be observed for mutations
+  var targetNode = document.getElementById('grids');
+
+  if (!targetNode) {
+    return;
+  }
+
+  // Options for the observer (which mutations to observe)
+  var config = { subtree: true, childList: true };
+
+  // Callback function to execute when mutations are observed
+  var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+      for (var added of mutation.addedNodes) {
+        if (added instanceof HTMLElement && added.matches(albumSelector)) {
+          setupContainer(added);
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  var observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+})();
